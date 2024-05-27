7f8a6b32e9cef47c3c44a29b3481cc0d92e454e0
Adrian Merle <adrian.merle@insa-lyon.fr>
2024-05-27 14:47:52 +0100

    Initial commit

---
+++ js/darkmode.js
@@ -0,0 +1,12 @@
+function toggleDarkMode() {
+    const theme = document.documentElement.getAttribute('data-bs-theme');
+    if (theme === 'dark') {
+        document.documentElement.setAttribute('data-bs-theme', 'light');
+        document.getElementById('modeIcon').classList.remove('fa-sun');
+        document.getElementById('modeIcon').classList.add('fa-moon');
+    } else {
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+        document.getElementById('modeIcon').classList.remove('fa-moon');
+        document.getElementById('modeIcon').classList.add('fa-sun');
+    }
+}
